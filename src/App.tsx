aa8ad45eb051d0689d8134729c15691b65682f8b
Luan Argolo <ipsluan@icloud.com>
2022-05-25 15:36:03 -0300

    feat: Add header and switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+
+import { GlobalStyles } from './styles/global';
+import Header from './components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyles />
+        <Header />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+import { shade } from 'polished';
+
+import { Container } from './styles';
+
+const Header = () => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      Hello World
+      <Switch
+        onChange={() => {}}
+        checked={false}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secondary}
+      />
+    </Container>
+  );
+};
+
+export default Header;
